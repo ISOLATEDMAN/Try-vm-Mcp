eafe004caecc1387b03ba5be07233e82c920c8d5
Kartikeya <samudralakartikeya@gmail.com>
2025-08-27 18:44:22 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from yoo!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ server.py
@@ -0,0 +1,12 @@
+from mcp.server import FastMCP
+
+mcp = FastMCP("YOO")
+
+@mcp.tool()
+def greeting():
+    return "welcome to the mcp"
+
+
+
+if __name__ == "__main__":
+    mcp.run("streamable-http" )
